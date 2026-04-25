552f7446a2ef2b83f33d5bd00a18d63f29af0020
GVL Apoorva <gvlapova3@gmail.com>
2026-04-25 23:52:53 +0530

    Biweekly Contest 181, Day 2

---
+++ Valid_Digit_Number.c
@@ -0,0 +1,33 @@
+bool validDigit(int n, int x) {
+    int res1 = 0, res2 = 0, rev = 0;
+    while(n != 0)
+    {
+        int last = n % 10;
+        if(last == x)
+        {
+            res1 = 1;
+            // break;
+        }
+        rev = rev * 10 + last;
+        n /= 10;
+    }
+
+    while(rev != 0)
+    {
+        int last = rev % 10;
+        if(last == x) 
+        {
+            break;
+        }
+        else 
+        {
+            res2 = 1;
+            break;
+        }
+        rev /= 10;
+    }
+    if (res1==1 && res2 == 1)
+        return true;
+    else 
+        return false;
+}
