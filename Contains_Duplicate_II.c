6d9b9b364f82556c21791995a79b60f9292caad3
GVL Apoorva <gvlapova3@gmail.com>
2026-05-26 23:34:29 +0530

    Contains Duplicate II
    Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.

---
+++ Contains_Duplicate_II.c
@@ -0,0 +1,14 @@
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    int j;
+    for(int i=0;i<numsSize;i++)
+    {
+        for(j=i+1;j<numsSize;j++)
+        {
+            int n=abs(i-j);
+            if(nums[i]==nums[j] && n<=k) return true;
+        }
+        if (numsSize > 10000)
+            i = j - 1;
+    }
+    return false;
+}
